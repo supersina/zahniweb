33601aa60f759f03265102c5185c23f781eaa11b
Sina Gierig <sinschkoli@gmail.com>
2021-04-20 14:03:21 +0200

    initial commit

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react';
+
+import 
+const overrides = {
+  styles,
+  borders,
+  // Other foundational style overrides go here
+  components: {
+    Button,
+    // Other components go here
+  },
+};
+export default extendTheme(overrides);
